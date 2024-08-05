8ed2790d8692d883a567c1978b5da6b58187c215
Chopra-14 <147584011+Chopra-14@users.noreply.github.com>
2024-08-05 15:59:40 +0530

    import java.util.Scanner; public class nod{     public static void main(String[] args){         int x,y;         Scanner read= new Scanner(System.in);         x=read.nextInt();         y=read.nextInt();         System.out.println(x*y);     } }

---
+++ Instant_Noodles.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class nod{
+    public static void main(String[] args){
+        int x,y;
+        Scanner read= new Scanner(System.in);
+        x=read.nextInt();
+        y=read.nextInt();
+        System.out.println(x*y);
+    }
+}
